431fda1417163f60dcb65fced49f76c2201cd54f
cyberforge12 <50057691+cyberforge12@users.noreply.github.com>
2021-04-09 00:17:26 +0300

    added total-information component

--- src/app/components/n-puzzle/n-puzzle.module.ts
+++ src/app/components/n-puzzle/n-puzzle.module.ts
@@ -6,9 +6,10 @@ import { NPuzzleComponent } from './n-puzzle.component';
 import { NPuzzleVisualizatorComponent } from './npuzzle-visualizator/npuzzle-visualizator.component';
 import { NPuzzleResultComponent } from './npuzzle-result/npuzzle-result.component';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { TotalInformationComponent } from './total-information/total-information.component';
 
 @NgModule({
-  declarations: [NPuzzleComponent, NPuzzleVisualizatorComponent, NPuzzleResultComponent],
+  declarations: [NPuzzleComponent, NPuzzleVisualizatorComponent, NPuzzleResultComponent, TotalInformationComponent],
   imports: [CommonModule, NPuzzleRoutingModule, MatExpansionModule],
   exports: [MatExpansionModule]
 })

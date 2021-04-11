67afdeac172173b67aed4a13baeefa0d7cd32b5f
Xel4ek <l-baal@yandex.ru>
2021-04-11 18:26:37 +0300

    meta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
- import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'n-puzzle';
+
+  constructor(private readonly meta: Meta) {
+    meta.addTags([
+      {
+        name: 'description',
+        content:
+          'Puzzle tag solver. 15 puzzle game. Artificial intelligence help in solving. School project 21 (42). The project was made using the Angular framework. All calculations are carried out in the browser. Completely Typescript project.',
+      },
+      { name: 'author', content: 'Xel4ek' },
+      {
+        name: 'keywords',
+        content: '15 puzzle, n puzzle, algorithm, 21 school, A*',
+      },
+    ]);
+  }
 }

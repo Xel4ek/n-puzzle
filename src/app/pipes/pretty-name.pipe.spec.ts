4e64e8e1e44be9644065aa6528037254d69ad508
Xel4ek <l-baal@yandex.ru>
2021-04-11 18:17:22 +0300

    meta

---
+++ src/app/pipes/pretty-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { PrettyNamePipe } from './pretty-name.pipe';
+
+describe('PrettyNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new PrettyNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/pretty-name.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'prettyName'
+})
+export class PrettyNamePipe implements PipeTransform {
+
+  transform(value: string, ...args: unknown[]): unknown {
+    return value.split(/(?=[A-Z])/).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+  }
+
+}
